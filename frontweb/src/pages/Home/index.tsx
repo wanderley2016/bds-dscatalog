e1ea56013b9ebc924e50fdb5993ed14f68f21b7e
wanderley <wanderleylandim@gmail.com>
2022-09-25 17:00:52 -0300

    Home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/buttonIcon';
 import Navbar from 'components/Navbar';
+import './styles.css';
 
 const Home = () => {
   return (
@@ -8,7 +10,14 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça o melhor catálago de produtos</h1>
+            <div>
+              <h1>Conheça o melhor catálago de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
             <MainImage />
