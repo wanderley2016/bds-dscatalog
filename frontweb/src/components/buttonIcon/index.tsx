36f96a0dbc26fecd2dd348c0bbd6ec574dfe3829
wanderley <wanderleylandim@gmail.com>
2022-10-23 14:22:02 -0300

    Custon ButtonIcon

--- frontweb/src/components/buttonIcon/index.tsx
+++ frontweb/src/components/buttonIcon/index.tsx
@@ -1,9 +1,15 @@
 import './styles.css';
-const ButtonIcon =() => {
-    return (
-        <button className="btn btn-primary btn-icon">
-        <h6>INICIE AGORA SUA BUSCA </h6>
-        </button>
-    )
-}
-export default ButtonIcon
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>inicie sua busca agora </h6>
+      </button>
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
+};
+export default ButtonIcon;
